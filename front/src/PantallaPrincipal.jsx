a01d1cf8aa6fbf0dec30f17b9bd181a1e76f34ef
Abigail05-ui <marianamirandagzz@outlook.com>
2025-03-04 20:10:47 -0600

    principal3

--- front/src/PantallaPrincipal.jsx
+++ front/src/PantallaPrincipal.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./PantallaPrincipal.css";
-import PantallaPrincipal from "./PantallaPrincipal";
+import NavbarPantallaPrincipal from "./NavbarPantallaPrincipal";
 import Footer from "./Footer";
 
 // Componente de Galería
