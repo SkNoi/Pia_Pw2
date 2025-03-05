ed44055c1571f192baae06ffcb180553d33442e2
Abigail05-ui <marianamirandagzz@outlook.com>
2025-03-04 23:37:33 -0600

    cambio posters

--- front/src/PantallaPrincipal.jsx
+++ front/src/PantallaPrincipal.jsx
@@ -1,24 +1,26 @@
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import "./Pantalla Principal.css";
+import "./PantallaPrincipal.css";
 import NavbarPantallaPrincipal from "./NavbarPantallaPrincipal";
 import Footer from "./Footer";
 
 // Componente de Galería
-
 const Galeria = () => {
-  const imagenes = ["/Recursos/Postersonic3.jpg", "/Recursos/Mario bros.jpg", "/Recursos/hxh.jpg"];
-  
+  const imagenes = [
+    "/Recursos/Postersonic3.jpg",
+    "/Recursos/Mario bros.jpg",
+    "/Recursos/hxh.jpg",
+  ];
+
   return (
-    <div className="PantallaPrincipal-Container">
-      <h2 className="text-2xl font-bold mb-4"></h2>
-      <div class="galeria" id="galeria"></div>
+    <div className="PantallaPrincipal-Container p-4">
       <div className="flex gap-4 flex-wrap">
         {imagenes.map((src, index) => (
           <img
             key={index}
             src={src}
             alt={`Imagen ${index + 1}`}
-            className="w-[150%] h-auto rounded-lg shadow-md object-cover"
+            className="w-40 h-auto rounded-lg shadow-md object-cover"
           />
         ))}
       </div>
