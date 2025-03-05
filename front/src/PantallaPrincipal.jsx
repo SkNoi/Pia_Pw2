b20bb6952be2e6d08148b34309efbd45a4e878ab
Abigail05-ui <marianamirandagzz@outlook.com>
2025-03-04 23:10:42 -0600

    poster9.6

--- front/src/PantallaPrincipal.jsx
+++ front/src/PantallaPrincipal.jsx
@@ -4,9 +4,10 @@ import NavbarPantallaPrincipal from "./NavbarPantallaPrincipal";
 import Footer from "./Footer";
 
 // Componente de Galería
+<div class="galeria" id="galeria"></div>
 const Galeria = () => {
   const imagenes = ["/Recursos/Postersonic3.jpg", "/Recursos/Mario bros.jpg", "/Recursos/hxh.jpg"];
-
+  
   return (
     <div className="PantallaPrincipal-Container">
       <h2 className="text-2xl font-bold mb-4"></h2>
